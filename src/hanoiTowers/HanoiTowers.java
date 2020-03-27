c1f2ef341b15bc79da4bde17a579566d37fc616b
toomec2105 <tomec2105@interia.pl>
2020-03-27 13:45:42 +0100

    initial commit

---
+++ src/hanoiTowers/HanoiTowers.java
@@ -0,0 +1,79 @@
+package hanoiTowers;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class HanoiTowers {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> first = new ArrayList<Integer>();
+		ArrayList<Integer> second = new ArrayList<Integer>();
+		ArrayList<Integer> third = new ArrayList<Integer>();
+		first.add(2);
+		first.add(1);
+		printTowers(first, second, third);
+		moveDisks(first, second, third);
+	}
+	
+	
+	
+	
+	
+	private static void moveDisks(ArrayList<Integer> first, ArrayList<Integer> second, ArrayList<Integer> third) {
+		
+		System.out.println("Moving disks");
+		int movedElement = 0;
+		    while(first.size() > 0 || second.size() > 0) {
+		    	
+		    	if(first.size() > 0) {//__--------------------------------------------
+		    		movedElement = first.get(first.size()- 1);
+		    		if(third.isEmpty()) {
+		    			third.add(movedElement);
+		    			first.remove(first.size()- 1);
+		    		}else if(movedElement < third.get(third.size() -1)) {
+		    			third.add(movedElement);
+		    			first.remove(movedElement);
+		    		}else if(second.isEmpty()) {
+	    				second.add(movedElement);
+		    			first.remove(first.size()- 1);
+	    			}
+		    		else if(movedElement < second.get(second.size() -1)) {
+		    			second.add(movedElement);
+		    			first.remove(first.size()- 1);
+		    		}else {
+		    			movedElement = third.get(third.size()- 1);	
+		    			if(second.isEmpty()) {
+		    				second.add(movedElement);
+			    			third.remove(third.size()- 1);
+		    			} else if(movedElement < second.get(second.size() -1)) {
+		    				second.add(movedElement);
+			    			third.remove(third.size()- 1);
+		    			}
+		    		}
+		    	}else if(second.size() > 0) {//__-------------------------------------------- 
+		    		
+		    	}
+		    	printTowers(first, second, third);
+		    }
+		
+			
+						
+			
+		
+		
+		
+	}
+	
+	private static void printTowers(ArrayList<Integer> first, ArrayList<Integer> second, ArrayList<Integer> third) {
+		
+		System.out.println(Arrays.toString(first.toArray()) + "First");
+		System.out.println(Arrays.toString(second.toArray()) + "Second");
+		System.out.println(Arrays.toString(third.toArray()) + "Third");
+	}
+	
+	
+	
+	
+	
+}
+
+	
